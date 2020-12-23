a8c0f3e2352ea8117a40867416eae540b6fbc6c4
sena-sekiguchi <t.r.a.a.y@icloud.com>
2020-12-24 01:37:53 +0900

    My Django Girls app, first commit

---
+++ sns/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Message,Friend,Group,Good,Menber,Category
+#from .models import Message,Friend,Good
+
+admin.site.register(Message)
+admin.site.register(Friend)
+admin.site.register(Group)
+admin.site.register(Good)
+admin.site.register(Menber)
+admin.site.register(Category)
+#sena@sena.sena
+#senasena
+
+#pass1010
